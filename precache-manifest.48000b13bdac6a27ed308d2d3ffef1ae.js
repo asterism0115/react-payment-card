3e276516a705c0bc703d39b56abf46aa124a82a3
Syed Simanta <syed.simanta06@gmail.com>
2020-01-08 20:44:15 +0600

    Updates

---
+++ precache-manifest.48000b13bdac6a27ed308d2d3ffef1ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2cd5e5b91ff18ec4c2d97ad4b48013",
+    "url": "/react-payment-card/index.html"
+  },
+  {
+    "revision": "d11bb0ad95d0c5fb447f",
+    "url": "/react-payment-card/static/css/main.8e33afb5.chunk.css"
+  },
+  {
+    "revision": "aa83da879a051bff6e2f",
+    "url": "/react-payment-card/static/js/2.faa0af17.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-payment-card/static/js/2.faa0af17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d11bb0ad95d0c5fb447f",
+    "url": "/react-payment-card/static/js/main.61e3ac8b.chunk.js"
+  },
+  {
+    "revision": "31f0a94845bfa976b81a",
+    "url": "/react-payment-card/static/js/runtime-main.3483d077.js"
+  }
+]);
